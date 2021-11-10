55faefe0b0e54597ea60773f819f7967678bc0e5
Ronny Leibham <ronnyleibham@gmx.de>
2021-11-10 12:43:42 +0100

    Ad Tags to Card component

--- src/app/components/Card/Card.stories.tsx
+++ src/app/components/Card/Card.stories.tsx
@@ -9,6 +9,7 @@ export default {
 const exampleContent = {
   name: 'Old Toothbrush',
   description: 'You could use it to clean shoes',
+  categories: ['german', 'movies'],
 };
 
 export const Regular = (): JSX.Element => <Card content={exampleContent} />;
--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
+import Tag from '../Tag/Tag';
 
 type CardProps = {
   content: {
     name: string;
     description: string;
+    categories: string[];
   };
 };
 
 export default function Card({ content }: CardProps): JSX.Element {
   return (
     <CardBody>
-      <h2>{content.name}</h2>
+      <CardHeading>{content.name}</CardHeading>
       <p>{content.description}</p>
+      {content.categories.map((category) => (
+        <Tag>{category}</Tag>
+      ))}
     </CardBody>
   );
 }
@@ -21,4 +26,8 @@ const CardBody = styled.article`
   padding: 1.5em;
   border: 5px solid #c92a2a;
   border-radius: 0.5em;
+`;
+
+const CardHeading = styled.h2`
+  color: #c92a2a;
 `;

b109f82ae435fa820d35d8195a2fbe93c5cf58ef
Ronny Leibham <ronnyleibham@gmx.de>
2021-11-11 10:18:26 +0100

    Last change

--- src/app/components/Card/Card.stories.tsx
+++ src/app/components/Card/Card.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import Card from './Card';
 
 export default {
@@ -6,26 +6,18 @@ export default {
   component: Card,
 };
 
-const exampleContentRegular = {
-  name: 'Old Toothbrush',
-  description: 'You could use it to clean shoes',
-};
-const exampleContentDetail = {
-  name: 'Old Toothbrush',
-  description: 'You could use it to clean shoes',
-  categories: ['18+', 'movies', 'action', 'german', 'fun', '2020'],
-};
+// const exampleContentRegular = {
+//   name: 'Old Toothbrush',
+//   description: 'You could use it to clean shoes',
+// };
+// const exampleContentDetail = {
+//   name: 'Old Toothbrush',
+//   description: 'You could use it to clean shoes',
+//   categories: ['18+', 'movies', 'action', 'german', 'fun', '2020'],
+// };
 
-export const Regular = (): JSX.Element => (
-    {things.length >= 0 &&
-        things.map((thing) => (
-          <Card
-            name={thing.name}
-            description={thing.description}
-            categories={thing.categories}
-          />
-        ))}
-);
-export const Detail = (): JSX.Element => (
-  <Card content={exampleContentDetail} />
-);
+// export const Regular = (): JSX.Element => (
+// );
+// export const Detail = (): JSX.Element => (
+
+// );
